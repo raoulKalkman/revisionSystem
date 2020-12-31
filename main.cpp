8a8e424b3713ceabc2ecc759540d32976d723a69
Raoul Kalkman <raoulkalkman@outlook.com>
2020-12-31 15:44:33 +0100

    add project description

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/* This program is made to test out ways to make a proper system to store all previous states of an object holding array
+ * for the purpose of testing I will use some very simple objects (which would be the same as nodes) it's more of a proof
+ * of concept than anything else */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
